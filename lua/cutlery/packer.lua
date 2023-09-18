7c1b6cd6503dd89949b40ce60696cac48b42fb40
Jonas Raneryd Imaizumi <jonas@mediaflow.com>
2023-09-18 23:22:03 +0200

    Get it up there

--- lua/cutlery/packer.lua
+++ lua/cutlery/packer.lua
@@ -12,5 +12,16 @@ return require('packer').startup(function(use)
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
+  use({
+	  'rose-pine/neovim',
+	  as = 'rose-pine',
+	  config = function()
+		  vim.cmd('colorscheme rose-pine')
+	  end
+  })
 
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('theprimeagen/harpoon')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
 end)
